9b8be1ecb0339815a1e689805312a265103371fb
Miguel Pedrosa <pedrosa.miguel@gmail.com>
2022-04-13 02:46:23 +0100

    Added simple load and store stream instructions

---
+++ riscv/insns/ss_st_d.h
@@ -0,0 +1,18 @@
+#define readRegAS(T, reg) static_cast<T>( READ_REG(reg) )
+
+auto streamReg = insn.uve_conf_rgD();
+auto baseReg = insn.uve_conf_rgBase();
+auto sizeReg = insn.uve_conf_rgN();
+auto strideReg = insn.uve_conf_rgStride();
+
+auto src = readRegAS(reg_t, baseReg);
+auto size = readRegAS(std::size_t, sizeReg);
+auto stride = readRegAS(std::size_t, strideReg);
+
+
+P.SU.registers[streamReg] = makeStreamRegister<std::uint64_t>(RegisterType::Store);
+operateRegister(P.SU, streamReg, [=](auto& reg) {
+    reg.startConfiguration({ src, size, stride });
+    reg.endConfiguration();
+});
+
