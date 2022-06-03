a3507fe124c2727e785eacd03589a87622d1a3f5
MiguelPedrosa <pedrosa.miguel@gmail.com>
2022-06-03 02:10:00 +0100

    Fixed issue with temporary destination

--- riscv/insns/so_v_mv.h
+++ riscv/insns/so_v_mv.h
@@ -1,7 +1,7 @@
-auto idxDest = insn.uve_comp_dest();
-auto idxSrc = insn.uve_conf_rgBase();
+auto& destReg = P.SU.registers[insn.uve_comp_dest()];
+auto& srcReg = P.SU.registers[insn.uve_conf_rgBase()];
 
-std::visit([](auto& dest, auto& src) {
+auto baseBehaviour = [](auto& dest, auto& src) {
     /* Streams can only output/input values if they are in the running status */
     // const bool runningcheck = src.getStatus() != RegisterStatus::Finished;
     // assert_msg("Stream was not configured to be running", runningcheck);
@@ -21,6 +21,32 @@ std::visit([](auto& dest, auto& src) {
       out.push_back(outPreStore);
     }
     dest.setElements(true, out);
+  };
+
+/* If the destination register is a temporary, we have to build it before the
+operation so that it's element size matches before any calculations are done */
+const bool makeTemporary = std::visit([](const auto& dest){
+  return dest.getStatus() != RegisterStatus::Running;
+}, destReg);
+if (makeTemporary) {
+  if (std::holds_alternative<StreamReg64>(srcReg)) {
+    destReg = makeStreamRegister<std::uint64_t>(RegisterType::Temporary);
+  } else if (std::holds_alternative<StreamReg32>(srcReg)) {
+    destReg = makeStreamRegister<std::uint32_t>(RegisterType::Temporary);
+  } else if (std::holds_alternative<StreamReg16>(srcReg)) {
+    destReg = makeStreamRegister<std::uint16_t>(RegisterType::Temporary);
+  } else if (std::holds_alternative<StreamReg8>(srcReg)) {
+    destReg = makeStreamRegister<std::uint8_t>(RegisterType::Temporary);
+  } else {
+    assert_msg("Trying to run so.v.mv with invalid src type", false);
+  }
+}
+
+std::visit(overloaded {
+    [&](StreamReg64& dest, StreamReg64& src) { baseBehaviour(dest, src); },
+    [&](StreamReg32& dest, StreamReg32& src) { baseBehaviour(dest, src); },
+    [&](StreamReg16& dest, StreamReg16& src) { baseBehaviour(dest, src); },
+    [&](StreamReg8&  dest, StreamReg8&  src) { baseBehaviour(dest, src); },
+    [&](auto& dest, auto& src) { assert_msg("Invoking so.v.mv with invalid parameter sizes", false); }
   },
-  P.SU.registers[idxDest],
-  P.SU.registers[idxSrc]);
+  destReg, srcReg);
